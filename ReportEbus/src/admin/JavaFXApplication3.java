6ec214d9ebf27fcaa41d96759da861efaf9c2d5c
fifa <59130500040@st.sit.kmutt.ac.th>
2017-04-18 16:42:03 +0700

    creat main admin

---
+++ ReportEbus/src/admin/JavaFXApplication3.java
@@ -0,0 +1,25 @@
+package admin;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class JavaFXApplication3 extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Admin.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        System.out.println("hello");
+        launch(args);
+    }
+    
+}
