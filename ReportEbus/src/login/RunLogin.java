5710ff334d68dbcef750af2b8143dc79ffb1624f
fifa <59130500040@st.sit.kmutt.ac.th>
2017-04-20 18:44:40 +0700

    Login GuiFinish

---
+++ ReportEbus/src/login/RunLogin.java
@@ -0,0 +1,29 @@
+package login;
+
+import register.*;
+import home.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RunLogin extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        
+        Scene scene = new Scene(root,800,600);
+        
+        stage.setTitle("Login");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        System.out.println("hello");
+        launch(args);
+    }
+    
+}
