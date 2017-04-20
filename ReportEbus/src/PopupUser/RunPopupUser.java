6afa87bc51a250e7cf52fea00bcbc7aba88ee498
fifa <59130500040@st.sit.kmutt.ac.th>
2017-04-20 19:39:42 +0700

    PopUp Gui Finish

---
+++ ReportEbus/src/PopupUser/RunPopupUser.java
@@ -0,0 +1,28 @@
+package PopupUser;
+
+import addProblem.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RunPopupUser extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("PopupUser.fxml"));
+        
+        Scene scene = new Scene(root,400,300);
+        
+        
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        System.out.println("hello");
+        launch(args);
+    }
+    
+}
