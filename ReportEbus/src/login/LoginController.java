3dd6034b53b795d17e2a67d6ced8c5e3584b3386
nattapat27 <nattapat27@gmail.com>
2017-04-28 16:44:51 +0700

    admin can login

--- ReportEbus/src/login/LoginController.java
+++ ReportEbus/src/login/LoginController.java
@@ -8,6 +8,18 @@ package login;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import ebus.model.Login;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +28,50 @@ import javafx.fxml.Initializable;
  */
 public class LoginController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TextField user;
+    @FXML
+    private TextField password;
+    @FXML
+    private Button loginButtton;
+    @FXML
+    private Button registerButton;
+    @FXML
+    private Text alretText;
+
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        alretText.setVisible(false);
     }    
+
+    @FXML
+    private void handleLoginAction(ActionEvent event) throws IOException{
+        String username = user.getText();
+        String pass = password.getText();
+        password.clear();
+        int ck = Login.cheak(username, pass);
+        if(ck==0){//wrong
+            alretText.setVisible(true);
+        }
+        else if(ck<0){//admin
+            Parent root = FXMLLoader.load(getClass().getResource("/admin/Admin.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setResizable(false);
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+            stage.show();
+        }
+        else{//user
+            
+        }
+    }
+
+    @FXML
+    private void handleRegisterAction(ActionEvent event) {
+        
+    }
     
 }
